3147c3cc6e20922eb37b6248a4c2851aef1e8971
Neil Chen <cheng.chen_1@nxp.com>
2022-01-06 16:01:47 +0800

    board: arm: Add board support for lpcxpresso55s36
    - Add pinmux, dts, doc
    - Tested samples: hello_world, philosophers, synchronization, basic/blinky, and basic/button.
    
    Signed-off-by: Neil Chen <cheng.chen_1@nxp.com>

---
+++ boards/arm/lpcxpresso55s36/board.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2021 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+board_runner_args(jlink "--device=LPC55S36" "--reset-after-load")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
