5829e41c55a9bf247e6ec90665af6171e930c001
Hake Huang <hake.huang@oss.nxp.com>
2022-06-22 17:33:18 +0800

    add i2c_shell example
    Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

---
+++ samples/drivers/i2c_shell/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+void main(void)
+{
+}
